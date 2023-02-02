220a3aec81582f162b67f8e845749396e2f141ae
corno <corno.astn.a@schraverus.com>
2023-02-02 10:49:49 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,8 +7,6 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
-    'name': "lib-pareto-filesystem",
-
     'author': "Corno",
     'description': "helper functions to make working with the filesystem easier than the low level functionality of res-pareto-filesystem",
     'license': "ISC",

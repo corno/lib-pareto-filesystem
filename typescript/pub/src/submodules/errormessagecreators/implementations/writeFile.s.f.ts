6c8194f18673eb42f82defed4b953e70a690dedc
corno <corno.astn.a@schraverus.com>
2023-04-05 05:29:30 +0200

    .

--- typescript/pub/src/submodules/errormessagecreators/implementations/writeFile.s.f.ts
+++ typescript/pub/src/submodules/errormessagecreators/implementations/writeFile.s.f.ts
@@ -5,7 +5,7 @@ import { A } from "../api.generated"
 export const $$: A.writeFile = () => {
     return ($) => {
         switch ($[0]) {
-            case 'no entity': return pl.ss($, ($) =>  `write file error: no entity`)
+            case 'no entity': return pl.ss($, ($) =>  `write file error: containing directory does not exist`)
             case 'is directory': return pl.ss($, ($) =>  `write file error: is directory`)
             case 'unknown': return pl.ss($, ($) =>  `unknown write file error: ${$.message}`)
             default: return pl.au($[0])
